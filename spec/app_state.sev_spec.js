bbdf35ad3c59b2cf32d88b95d7700a47dad628ed
guy <guy@spectory.com>
2015-12-10 23:40:14 +0200

    validators

--- spec/app_state.sev_spec.js
+++ spec/app_state.sev_spec.js
@@ -1,12 +1,12 @@
 /*global angular, _, inject, describe, beforeEach, expect, it */
 'use strict';
-describe('AppState,', function () {
+describe('StateService,', function () {
   var service, state;
 
   beforeEach(angular.mock.module('app'));
 
-  beforeEach(angular.mock.inject(function (AppState) {
-    service = AppState;
+  beforeEach(angular.mock.inject(function (StateService) {
+    service = StateService;
     state = {};
     service.mockState(state);
   }));
@@ -16,41 +16,73 @@ describe('AppState,', function () {
   });
 
   describe(', defineState', function () {
-
     describe(', valid inputs', function () {
       describe('defenition with name only', function () {
         it(', should define a state attribute by given name', function () {
           service.defineState({name: 'selected_header_tab'});
           expect(state.selected_header_tab).toBe(undefined);
         });
-        it(', should define a state attribute by given name', function () {
-          var dummyValidator = function () { return true; };
-          service.defineState({name: 'selected_header_tab', validator: dummyValidator});
-          expect(state.selected_header_tab).toBe(undefined);
-        });
+
         it(', should throw error if attribute was already defined', function () {
-          var dummyValidator = function () { return true; };
-          service.defineState({name: 'selected_header_tab', validator: dummyValidator});
+          service.defineState({name: 'selected_header_tab'});
           var test = function () {service.defineState({name: 'selected_header_tab'}); };
           expect(test).toThrow();
+
+        });
+      });
+
+      describe(', defenition with name and validator function', function () {
+        var dummyValidator = function () { return true; };
+        it(', should define a state attribute by given name', function () {
+          service.defineState({name: 'selected_header_tab', validator: dummyValidator});
+          expect(state.selected_header_tab).toBe(undefined);
         });
       });
     });
 
     describe(', invalud inputs', function () {
+
       it(', should raise error when given state attribute is not a non empty String', function () {
-        var invalid_attrs = [1, null, {}, undefined, ''];
+        var invalid_attrs = [
+          1,
+          null,
+          undefined,
+          {},
+          '',
+          '    string_that_start_with_spaceing',
+          '1123',
+          '3string_that_start_with_numbers',
+          'string with spaces',
+          {name: 'valid_name', validator: 'notAfunction'}
+        ];
         var tests = _.map(invalid_attrs, function (a) {
           return function () { service.defineState(a); };
         });
         _.forEach(tests, function (t) { expect(t).toThrow(); });
       });
+
+    });
+  });
+
+  describe('set', function () {
+    beforeEach(function () {
+      var validator = function (x) {
+        var acceptable_values = ['home', 'settings', 'admin'];
+        return acceptable_values.indexOf(x) >= 0;
+      };
+      service.defineState({name: 'selected_header_tab', validator: validator});
+    });
+
+    it('when value is valid', function () {
+      service.set({name: 'selected_header_tab', value: 'home'});
+      expect(state.selected_header_tab).toEqual('home');
+    });
+
+    it('when value is invalid', function () {
+      var test = function () { service.set({name: 'selected_header_tab', value: 'somthing else'}); };
+      expect(test).toThrow();
     });
   });
 
 });
 
-// var validator = function (x) {
-//   var acceptable_values = ['home', 'settings', 'admin'];
-//   return acceptable_values.indexOf(x) >= 0;
-// };
--- src/app_state.srv.js
+++ src/app_state.srv.js
@@ -1,15 +1,23 @@
 /*globals angular, _ */
 'use strict';
-angular.module('app').factory('AppState', function () {
+
+angular.module('app').factory('StateService', function () {
   var state;
   var service = {};
+  var VALIDATORS = {};
   var VALID_VAR_RGX = /[a-zA-Z_][a-zA-Z0-9_]*/;
+
+  function T() { return true; }
 
   function invalid_def(def) {
     if (!def) { return true; }
     if (!def.name) { return true; }
     if (!VALID_VAR_RGX.test(def.name)) { return true; }
     return false;
+  }
+
+  function valid_validator(v) {
+    return typeof v === "function";
   }
 
   function already_defined(key) {
@@ -23,7 +31,16 @@ angular.module('app').factory('AppState', function () {
   service.defineState = function (def) {
     if (invalid_def(def)) {throw [def, "is an invalid state definition"].join(' '); }
     if (already_defined(def.name)) {throw [def.name, "was already defined"].join(' '); }
+    if (def.validator && !valid_validator(def.validator)) {throw [def, "Invalid validator"].join(' '); }
+    VALIDATORS[def.name] = def.validator || T;
     state[def.name] = undefined;
+  };
+
+  service.set = function (key_and_value) {
+    var key = key_and_value.name;
+    var val = key_and_value.value;
+    if (!VALIDATORS[key](val)) {throw [val, "is an invalid value for", key].join(' '); }
+    state[key] =  val;
   };
 
   return service;
